9ee14d4e0264695cfb1e0ddfd57754552cbefa6b
Rayan84 <rayan_84@live.com>
2022-04-12 13:16:22 +0200

    Add random selection functionality

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -3,7 +3,9 @@ class GreetingsController < ApplicationController
 
   # GET /greetings
   def index
-    @greetings = Greeting.all
+    #@greetings = Greeting.all
+    @greetings = Greeting.all.sample
+
 
     render json: @greetings
   end
